4db503d35276e38c78883dd51a3f6bafe6ca6543
rayhan-bapari <mdrayhanbapari02@gmail.com>
2025-01-12 00:11:56 +0600

    added swiper and responsive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,7 +6,7 @@
         </div>
         <nav class="navbar">
             <ul>
-                <li><a href="#">Home</a></li>
+                <li><a href="#" class="active">Home</a></li>
                 <li><a href="#">About us</a></li>
                 <li><a href="#">Projects</a></li>
                 <li><a href="#">Agents</a></li>
